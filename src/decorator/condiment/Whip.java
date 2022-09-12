774d4a74a370d076ea7521e40ce682b3d16f956c
DH <donghaim71@gmail.com>
2022-09-12 20:03:09 +0900

    add Beverage, Condiment

---
+++ src/decorator/condiment/Whip.java
@@ -0,0 +1,21 @@
+package decorator.condiment;
+
+import decorator.beverage.Beverage;
+
+public class Whip extends CondimentDecorator {
+
+  public Whip(Beverage beverage) {
+    this.beverage = beverage;
+  }
+
+  @Override
+  public String getDescription() {
+    return beverage.getDescription() + ",휘핑크림";
+  }
+
+  @Override
+  public double cost() {
+    return beverage.cost() + 0.10;
+  }
+
+}
